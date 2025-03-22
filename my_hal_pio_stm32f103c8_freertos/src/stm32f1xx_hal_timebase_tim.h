6e035954836a178d91b080374480ded105a593cd
tuf-u24 <2514034568@qq.com>
2025-03-22 16:56:35 +0800

    change time base TIM and add img for freertos

--- my_hal_pio_stm32f103c8_freertos/src/stm32f1xx_hal_timebase_tim.h
+++ my_hal_pio_stm32f103c8_freertos/src/stm32f1xx_hal_timebase_tim.h
@@ -7,9 +7,9 @@
 extern TIM_HandleTypeDef        htim_timebase;
 // void TIM3_IRQHandler(void);
 /*************time base source config*************/
-#define TIM_TIMEBASE                      TIM2
-#define TIM_TIMEBASE_CLK_ENABLE()         __HAL_RCC_TIM2_CLK_ENABLE()
-#define TIM_TIMEBASE_IRQHandler(void)     TIM2_IRQHandler(void)
-#define TIM_TIMEBASE_IRQn                 TIM2_IRQn
+#define TIM_TIMEBASE                      TIM3
+#define TIM_TIMEBASE_CLK_ENABLE()         __HAL_RCC_TIM3_CLK_ENABLE()
+#define TIM_TIMEBASE_IRQHandler(void)     TIM3_IRQHandler(void)
+#define TIM_TIMEBASE_IRQn                 TIM3_IRQn
 
 #endif
